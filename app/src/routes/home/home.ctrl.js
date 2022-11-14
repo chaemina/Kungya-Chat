5ac709c7f996389079e7e13fb0d367bb02ea4f95
yuseonkim <yusun715@icloud.com>
2022-11-14 16:10:44 +0900

    Express 적용

---
+++ app/src/routes/home/home.ctrl.js
@@ -0,0 +1,6 @@
+
+const output = {
+    home : (req,res) =>{
+    res.render("home/index");
+    },
+};
---
+++ app/src/routes/home/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const express =require ("express");
+const router = express.Router();
+const ctrl = require ('./home.ctrl');
+
+router.get("/",ctrl.output.home);
